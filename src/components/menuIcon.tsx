6563d7f32b20f986a260281126ad19bee953f517
Geórgia Carin Martins da Silva <georgiacarin@gmail.com>
2025-08-26 16:39:32 -0300

    Style: criando sections do site

---
+++ src/components/menuIcon.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import { Box } from '@mui/material';
+export const MenuIcon = () => {
+  return (
+    <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 2,
+    }}>
+
+        <EmailOutlinedIcon sx={{
+            border: '2px solid #aaa',
+            padding: 1,
+            color: '#aaa',
+            borderRadius: '20%',
+        }} />
+        <EmailOutlinedIcon sx={{
+            border: '2px solid #aaa',
+            color: '#aaa',
+            padding: 1,
+            borderRadius: '20%',
+        }} />
+        <EmailOutlinedIcon sx={{
+            border: '2px solid #aaa',
+            color: '#aaa',
+            padding: 1,
+            borderRadius: '20%',
+        }} />
+        <EmailOutlinedIcon sx={{
+            border: '2px solid #aaa',
+            color: '#aaa',
+            padding: 1,
+            borderRadius: '20%',
+        }} />
+        <EmailOutlinedIcon sx={{
+            border: '2px solid #aaa',
+            color: '#aaa',
+            padding: 1,
+            borderRadius: '20%',
+        }} />
+      
+    </Box>
+  )
+}
---
+++ src/sections/skills.tsx
@@ -0,0 +1,62 @@
+import { Box, Container, Grid, Typography } from '@mui/material'
+import React from 'react'
+import figma from '../assets/figma.svg'
+import react from "../assets/react.svg"
+import wordpress from "../assets/wordpress.svg"
+export const Habilidade = () => {
+
+  return (
+    <Box sx={{
+      paddingTop: '100px',
+      paddingBottom: '100px',
+      backgroundColor: '#0f0716',
+      borderRadius: '8px',
+    }}>
+      <Typography variant="h5" component="h2" sx={{
+        color: '#fff',
+        fontWeight: 'Bold',
+        textAlign: 'center',
+      }}>
+        Minhas Habilidades
+      </Typography>
+      <Typography variant="h6" sx={{
+        color: '#ccc',
+        marginTop: '8px',
+        textAlign: 'center',
+
+        marginLeft: 'auto',
+        marginRight: 'auto',
+      }}>
+        Aqui estão algumas das minhas habilidades técnicas:
+      </Typography>
+      <Container> 
+
+        <Grid container spacing={4} >
+          {[figma, react, wordpress].map((skill, index) => (
+          <Grid size={3} sx={{
+            backgroundColor: '#ffffff13',
+            borderRadius: '24px',
+            padding: '50px',
+
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 1,
+            marginTop: '100px',
+            marginBottom: '100px',
+          }}>
+            <img src={skill} alt="" style={{ width: '140px',  }} />
+            <Typography variant="h6" sx={{
+              color: '#fff',
+              textAlign: 'center',
+              fontWeight: 'Bold',
+            }}>
+              Figma
+            </Typography>
+          </Grid>
+        ))}
+        </Grid>
+      </Container>
+    </Box>
+  )
+}
